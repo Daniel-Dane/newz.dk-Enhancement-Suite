c7c34c971f325b23b0c8d7b07c34d93e264e4188
Daniel-Dane <dan_nysen@yahoo.dk>
2011-09-23 22:58:34 +0200

    Signed-off-by: Daniel-Dane <dan_nysen@yahoo.dk>

--- nes.user.js
+++ nes.user.js
@@ -4,38 +4,40 @@
 // @description   newz.dk er nu endnu mere perfekt!
 // @include       http://newz.dk/*
 // @include       http://*.newz.dk/*
+// @exclude       http://newz.dk/banner/*
+// @exclude       http://*.newz.dk/banner/*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // @version       0.1
 // ==/UserScript==
 
-function init() {
-	//content = '<div style="font-size: 12pt"><input type="checkbox" id="kage" name="kage"><label for="kage"> test</label></div>';
-	
+function init() {	
 	$('<div/>').insertAfter('#nmTopBar').css('font-size', '12pt').html('<input type="checkbox" id="kage" name="kage"><label for="kage"> test</label>');
 
 	//if $.Storage.get("ajaxPageChange")
 		ajaxPageChange();
 }
 
+	function insertLoadingGif() {
+		// Indsætter en loading.gif
+		alert('123');
+		$('<span/>').insertAfter('.pagination').html('<div class="loading" style="float: left; margin: -2px 10px; padding: 5px; position: relative; width: 330px;"><p><img src="http://d9projects.com/loading.gif" /> Weeeeeeeeee.</p></div>');
+		$(".loading").ajaxStart(function() {
+			$('.pagination').hide();
+			$(this).show();
+		}).ajaxStop(function() {
+			$(this).hide();
+			$('.pagination').show();
+		}).hide();
+		alert('321');
+	}
+
 function ajaxPageChange() {
-	// Indsætter en loading.gif
-	// Forsvinder efter første fAJAX, da den ikke er .live (jeg retter det senere)
-	content = '<div class="loading" style="float: left; margin: -2px 10px; padding: 5px; position: relative; width: 330px;"><p><img src="http://d9projects.com/loading.gif" /> Weeeeeeeeee.</p></div>';
-	$(content).insertAfter('.pagination');
-	$(".loading").ajaxStart(function(){
-		$('.pagination').hide();
-		$(this).show();
-	}).ajaxStop(function(){
-		$(this).hide();
-		$('.pagination').show();
-	}).hide();
+	insertLoadingGif();
 	
-	$('.pagination a').live('click', function(){
+	$('.pagination a').live('click', function() {
 		$.ajax({
 			url: $(this).attr('href'),
 			success: function (data) {
-				//window.testa = data; // debug
-				
 				// Henter title (side og titel på tråd), som indsættes i <title> og <h1>
 				// newz.dk sætter normalt kun side ind i <h1>, når man skifter side, tsk tsk
 				re = /<title>(.+)&raquo;.+&raquo;.+<\/title>/;
@@ -44,24 +46,23 @@ function ajaxPageChange() {
 				$("title").html(a);
 				$("#postcontainer").html($(data).find("#postcontainer").html());
 				
-				// Sætter hash til første indlæg, så man kan kopiere link til den rette side
-				window.location.hash = $("#comments > div:first-child h2 a:first-child").attr('name');
+				// Sætter hash til første indlæg, så man kan kopiere link til den rette side -- irriterende, så nvm
+				//window.location.hash = $("#comments > div:first-child h2 a:first-child").attr('name');
 				
 				// Opdaterer newz.dk's variable, så den kun henter nye indlæg, når man er på sidste side
 				re = /_pageId = (\d+);/;
-				_pageId = parseInt(re.exec(data)[1]);
+				_pageId = +(re.exec(data)[1]);
 				re = /_lastPage = (\d+);/;
-				_lastPage = parseInt(re.exec(data)[1]);
+				_lastPage = +(re.exec(data)[1]);
 				if (_pageId != _lastPage)
 					PauseAutoUpdate()
 				else
 					StartAutoUpdate();
-				
-				// (Gen)aktiverer js for "Yderligere information", etc. ved at sætte event handlers igen (newz.dk-funktion)
+
+				// (Gen)aktiverer js for "Yderligere information", etc. ved at sætte event handles igen (newz.dk-funktion)
 				UpdatePosts();
 				
-				// Sætter event handles for de nye knapper -- lige meget med .live
-				//ajaxPageChange();
+				insertLoadingGif();
 			}
 		});
 		return false;
