302208d42fb9c385755987eeef24e98cbb59cb35
Daniel-Dane <dan_nysen@yahoo.dk>
2011-10-30 17:54:31 +0100

    test uhm, 4?

--- nes.js
+++ nes.js
@@ -13,12 +13,12 @@ if (!localStorage) {
 } else {
 	if ((/^http:\/\/(.+\.)?newz\.dk(?!\/banner).*$/.test(location.href)) && (!NES_loaded)) {
 		NES_loaded = true;
-		var startHash = location.hash; // Gemmer hash, hvis newz.dk AJAX'er til den rigtige side, så vi kan hoppe til det rigtige indlæg
-		var startPage = 0;             // Bruges af "Sideskift ved henvisning til indlæg på anden side"
-		var startScroll = 0;           // Bruges også af ovenstående
-		var postSortByRating = false;  // true, når der er trykket på "Sorter indlæg efter rating"
+		var startHash = location.hash;  // Gemmer hash, hvis newz.dk AJAX'er til den rigtige side, så vi kan hoppe til det rigtige indlæg
+		var startPage = 0;              // Bruges af "Sideskift ved henvisning til indlæg på anden side"
+		var startScroll = 0;            // Bruges også af ovenstående
+		var postSortByRating = false;   // true, når der er trykket på "Sorter indlæg efter rating"
 		$(document).ready(function() {
-			startPage = _pageId || -1;
+			startPage = window._pageId; // Kommer først senere. Måske. Bruger window, så startPage bliver undefined, hvis den ikke findes (uden window, failer koden).
 			init();
 		});
 	}
