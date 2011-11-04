56777257c93650e79ce837716366949c59ed4b20
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-04 11:15:37 +0100

    .

--- nes.js
+++ nes.js
@@ -2,8 +2,8 @@
  @name           newz.dk Enhancement Suite
  @url            https://raw.github.com/Daniel-Dane/newz.dk-Enhancement-Suite/master/nes.js
 */
-NES_version = "2.0 alpha";
-NES_loaded = NES_loaded || false;
+var NES_version = "2.0 alpha";
+var NES_loaded = NES_loaded || false;
 
 // Følgende indsættes i indstillinger -> stylesheet
 // " /><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script><script type="text/javascript" src="https://raw.github.com/Daniel-Dane/newz.dk-Enhancement-Suite/master/nes.js"></script><link rel="stylesheet
