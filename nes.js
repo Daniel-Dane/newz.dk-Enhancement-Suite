665e22e0340bf2af739256bb0170969493b5bf4d
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-05 23:38:54 +0100

    .

--- nes.js
+++ nes.js
@@ -346,7 +346,7 @@ function NES_addLinkToPostReferenceFunc(object) {
 				if (this.nodeType == 3) {
 					// #tal efterfulgt af enten mellemrum, linjeknæk, kolon, komma eller punktum samt ved afsluttet afsnit eller linje
 					$(this).replaceWith(this.nodeValue.replace(/#(\d+)( |<br>|:|,|\.|\?|!|<\/p>|$)/gm, function(str, a, b) {
-						if (a == 0 || a == postNum)
+						if (a == 0 || (a == postNum && p != 'post_preview'))
 							return '#' + a + b;
 						if (a < 100 && _pageId > 20) { // Fra indlæg #1001 vil #99 betyder #999 osv.
 							c = Math.floor((50 * (_pageId - 1)) / 100) * 100 + +a;
