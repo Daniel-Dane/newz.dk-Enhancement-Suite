6a28f3cf10d900ef2b01f3679154c39a5af2e522
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-13 16:17:14 +0100

    .

--- nes.js
+++ nes.js
@@ -121,6 +121,7 @@ function NES_init() {
 				history.replaceState({page: _lastPage}, '', href + '/page' + _lastPage);
 				NES_fetchPage(_lastPage, 0);
 			} else {
+				console.log('test');
 				if (NES_startHash != '') {
 					$(window).scrollTop($('.comment h2:has(a[name=' + NES_startHash.substr(1) + '])').offset().top);
 					history.replaceState({page: _pageId}, '', href + '/page' + _pageId + NES_startHash);
