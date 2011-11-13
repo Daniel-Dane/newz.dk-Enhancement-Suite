4469760a6a61580580cdb910d5178f66f9926bc5
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-13 14:42:43 +0100

    .

--- nes.js
+++ nes.js
@@ -371,6 +371,7 @@ function NES_addLinkToPostReferenceFunc(object) {
 				if (this.nodeType == 3) {
 					// #tal efterfulgt af enten mellemrum, linjeknæk, kolon, komma, punktum, spørgsmålstegn eller udråbstegn samt ved afsluttet afsnit eller linje
 					$(this).replaceWith(this.nodeValue.replace(/#(\d+)( |<br>|:|,|\.|\?|!|<\/p>|$)/gm, function(str, a, b) {
+						a = +a;
 						if (a < 100 && _pageId > 20) { // Fra indlæg #1001 vil #99 betyder #999 osv.
 							c = Math.floor((50 * (_pageId - 1)) / 100) * 100 + +a;
 							if (c > postNum)
