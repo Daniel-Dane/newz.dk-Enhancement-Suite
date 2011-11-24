c10d91719f37cdfb832576865a7a6f8c0cfb556f
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-24 17:28:27 +0100

    .

--- nes.js
+++ nes.js
@@ -737,8 +737,6 @@ function NES_improvedQuote(object) {
 										switch (obj.attr.data) {
 											case 'NES_img':
 												s = $('<div><a href="'+obj.attr.href+'">'+obj.attr.href+'</a></div>').linkShorten().find('a').text();
-												console.log(s);
-												console.log(obj.attr.title);
 												if (s === obj.attr.title || s.substr(7) === obj.attr.title)
 													t += obj.attr.href;
 												else
