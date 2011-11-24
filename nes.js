5844dea67e958b8c89575221476f08e607e92bfc
Daniel-Dane <dan_nysen@yahoo.dk>
2011-11-24 17:15:46 +0100

    .

--- nes.js
+++ nes.js
@@ -751,7 +751,7 @@ function NES_improvedQuote(object) {
 												l = s.indexOf('</');
 												// a uden [url], else a med [url]
 												console.log((s.substr(l, 4) === '</a>'));
-												console.log($('<div><a href="'+obj.attr.href+'">'+obj.attr.href+'</a></div>').linkShorten().find('a').text());
+												console.log($('<div><a href="'+obj.attr.href+'">'+obj.attr.href+'</a></div>').linkShorten().find('a').text().substr(7));
 												console.log(s.substring(s.indexOf('>') + 1, l));
 												if (s.substr(l, 4) === '</a>' && $('<div><a href="'+obj.attr.href+'">'+obj.attr.href+'</a></div>').linkShorten().find('a').text() === s.substring(s.indexOf('>') + 1, l)) {
 													t += obj.attr.href;
