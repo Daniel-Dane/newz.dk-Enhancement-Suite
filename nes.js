039594bc5744d1b89f653a9d25942104b442053c
Daniel-Dane <dan_nysen@yahoo.dk>
2011-12-05 11:13:39 +0100

    .

--- nes.js
+++ nes.js
@@ -380,11 +380,15 @@ function NES_init() {
 	
 	// Statistik
 	if (localStorage["NES_stats"] != "true") {
-		var un = $('#nmProfile').next().attr('href');
-		un = un.substr(un.lastIndexOf('/') + 2);
-		$.getScript('http://d9projects.com/NES_stats.php?NES&username='+un+'&addLinkToPostReference='+ +addLinkToPostReference+'&showPostOnMouseOverReference='+ +showPostOnMouseOverReference+'&showPostOnMouseOverReferenceLeft='+ +(localStorage["showPostOnMouseOverReferenceLeft"] == "true")+'&showPostOnMouseOverReferenceMini='+ +showPostOnMouseOverReferenceMini+'&improvedQuoteSetting='+ +improvedQuoteSetting+'&applyTargetBlank='+ +applyTargetBlank+'&applyTargetBlankOnlyOutgoing='+ +applyTargetBlankOnlyOutgoing+'&fixFailTagsSetting='+ +fixFailTagsSetting+'&showUrlImages='+ +showUrlImages+'&embedYouTubeUrls='+ +embedYouTubeUrls+'&embedYouTubeUrlsNotInQuote='+ +embedYouTubeUrlsNotInQuote, function(){});
-		localStorage["NES_stats"] = 'true';
+		uploadStatistik();
 	}
+}
+
+function uploadStatistik() {
+	var un = $('#nmProfile').next().attr('href');
+	un = un.substr(un.lastIndexOf('/') + 2);
+	$.getScript('http://d9projects.com/NES_stats.php?NES&hash='+localStorage["NES_statHash"]+'&username='+un+'&addLinkToPostReference='+ +addLinkToPostReference+'&showPostOnMouseOverReference='+ +showPostOnMouseOverReference+'&showPostOnMouseOverReferenceLeft='+ +(localStorage["showPostOnMouseOverReferenceLeft"] == "true")+'&showPostOnMouseOverReferenceMini='+ +showPostOnMouseOverReferenceMini+'&improvedQuoteSetting='+ +improvedQuoteSetting+'&applyTargetBlank='+ +applyTargetBlank+'&applyTargetBlankOnlyOutgoing='+ +applyTargetBlankOnlyOutgoing+'&fixFailTagsSetting='+ +fixFailTagsSetting+'&showUrlImages='+ +showUrlImages+'&embedYouTubeUrls='+ +embedYouTubeUrls+'&embedYouTubeUrlsNotInQuote='+ +embedYouTubeUrlsNotInQuote, function(){});
+	localStorage["NES_stats"] = 'true';
 }
 
 function NES_updateSettingsSub() {
